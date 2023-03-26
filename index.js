884ef996be1071ddb6ccb6392e304aeb9589e930
Ahmad Ramin Soleyman kheyl <sramin963@gmail.com>
2023-03-27 00:15:40 +0300

    Assignment is done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'someone else';
+}
